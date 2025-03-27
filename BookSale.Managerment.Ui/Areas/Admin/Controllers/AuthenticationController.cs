fe45cd8faca2b496fb5384eeb20d883edd4a66db
vuan201 <vuan201@gmail.com>
2025-03-27 13:48:14 +0700

    Exercise[22] create login page

---
+++ BookSale.Managerment.Ui/Areas/Admin/Controllers/AuthenticationController.cs
@@ -0,0 +1,34 @@
+﻿using BookSale.Managerment.Domain.Abstract;
+using BookSale.Managerment.Ui.Areas.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookSale.Managerment.Ui.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AuthenticationController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public AuthenticationController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View(new LoginForm());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login(LoginForm loginForm)
+        {
+            return View();
+        }
+    }
+}
---
+++ BookSale.Managerment.Ui/Areas/Admin/Models/LoginForm.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookSale.Managerment.Ui.Areas.Admin.Models
+{
+    public class LoginForm
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        [MinLength(6, ErrorMessage = "Mật khẩu phải tối thiểu 6 ký tự")]
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+    }
+}
