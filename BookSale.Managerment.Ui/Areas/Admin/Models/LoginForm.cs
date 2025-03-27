3237ea7c1a3c0948729cf7f00b6a380a07b6eb24
vuan201 <vuan201@gmail.com>
2025-03-27 15:48:49 +0700

    Exercise[23]: validate form login

--- BookSale.Managerment.Ui/Areas/Admin/Models/LoginForm.cs
+++ BookSale.Managerment.Ui/Areas/Admin/Models/LoginForm.cs
@@ -4,9 +4,9 @@ namespace BookSale.Managerment.Ui.Areas.Admin.Models
 {
     public class LoginForm
     {
-        [Required]
+        [Required(ErrorMessage = "Vui lòng nhập tên đăng nhập")]
         public string Username { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Vui lòng nhập mật khẩu")]
         [MinLength(6, ErrorMessage = "Mật khẩu phải tối thiểu 6 ký tự")]
         public string Password { get; set; }
         public bool RememberMe { get; set; }
