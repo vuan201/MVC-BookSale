86d5e26658182a13b22ff6574e9b532fcab4649c
vuan201 <vuan201@gmail.com>
2025-04-25 15:57:37 +0700

    Exercise[33]: handle image preview when uploading image

---
+++ BookSale.Managerment.Ui/wwwroot/js/changeAvatarEvent.js
@@ -0,0 +1,21 @@
+﻿const avatarImg = document.querySelector("#avatar-img");
+const fileImgInput = document.getElementById("file-input");
+
+fileImgInput.addEventListener("change", function () {
+    const input = this.files[0];
+
+    if (input) {
+        avatarImg.classList.remove("no-image"); // Remove trước khi load
+        avatarImg.src = URL.createObjectURL(input);
+    }
+});
+
+avatarImg.addEventListener("error", function () {
+    onErrorImage();
+});
+
+function onErrorImage() {
+    avatarImg.src = "~/image/no_image.jpg";
+    avatarImg.alt = "Default Avatar";
+    avatarImg.classList.add("no-image"); // Thêm lại nếu ảnh lỗi
+}
